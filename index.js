3cfc601ce165cae144977b9e05a08210c9fbb146
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2024-03-31 02:13:03 +0300

    src updated
    screens catagorized

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './CaregoriesScreen';
+import App from './src/screens/CaregoriesScreen';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
